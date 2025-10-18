c422ee4895e0f990973996da9e1522a87ace2445
edman3d <ejsmajestic@gmail.com>
2025-10-18 09:18:35 -0400

    high level fetch function to handle localhost and auth bypass on vercel environments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { HydratedDocument } from 'mongoose';
 import { CivType } from "@/models/Civ";
-
+import { fetchData } from "@/lib/fetchData";
 // export const metadata = {
 //   title: "Incremental Static Regeneration - NextJS 13.4 Image Gallery",
 // }
@@ -17,14 +17,13 @@ export default async function Home() {
   // const civResponse = await fetch('http://localhost:3000/api/civs');
   console.log('check if using preview mongodb_uri now');
   console.log('NEXT_PUBLIC_BASE_URL', process.env.NEXT_PUBLIC_BASE_URL); // undefined in preview build
-
   console.log('VERCEL_ENV :>> ', process.env.VERCEL_ENV);
   console.log('VERCEL_URL :>> ', process.env.VERCEL_URL);
   console.log('VERCEL_PROJECT_PRODUCTION_URL :>> ', process.env.VERCEL_PROJECT_PRODUCTION_URL);
   console.log('VERCEL_BRANCH_URL :>> ', process.env.VERCEL_BRANCH_URL);
   console.log('VERCEL_AUTOMATION_BYPASS_SECRET :>> ', process.env.VERCEL_AUTOMATION_BYPASS_SECRET);
-  const civResponse = await fetch(`https://${process.env.VERCEL_URL}/api/civs?x-vercel-protection-bypass=${process.env.VERCEL_AUTOMATION_BYPASS_SECRET}`);
-  // const civResponse = await fetch('/api/civs');
+
+  const civResponse = await fetchData(`${process.env.VERCEL_URL}/api/civs`);
   const civData: CivType[] = await civResponse.json();
   console.log(civData[0].army_type);
 
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,6 +1,9 @@
 import { cleanEnv } from "envalid";
 import { port, str } from "envalid/dist/validators";
 
+if (process.env.NODE_ENV !== 'production') {
+    process.env.VERCEL_URL = 'localhost:3000';
+}
 
 export default cleanEnv(process.env, {
     NODE_ENV: str({ choices: ['development', 'production'] }),
