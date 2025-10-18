bbe48ea37f286e0183145e9b80cfba1307991868
edman3d <ejsmajestic@gmail.com>
2025-10-18 07:52:08 -0400

    set no caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ import { CivType } from "@/models/Civ";
 //   title: "Incremental Static Regeneration - NextJS 13.4 Image Gallery",
 // }
 
-export const revalidate = 3600;
+export const revalidate = 0;
 
 // For more granular control, you can implement on-demand revalidation. This involves creating an API Route 
 // (now called Route Handlers in the App Router) that triggers revalidation for a specific path or cache tag
 //  using revalidatePath or revalidateTag from next/cache.
 
 export default async function Home() {
-  const civResponse = await fetch('http://localhost:3000/api/civs');
-  // const civResponse = await fetch('/api/civs');
+  // const civResponse = await fetch('http://localhost:3000/api/civs');
+  const civResponse = await fetch('/api/civs');
   const civData: CivType[] = await civResponse.json();
   console.log(civData[0].army_type);
 
