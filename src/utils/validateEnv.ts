d05ec2bc1c04eda75ff2e9dda6bf52f96f0a24fc
edman3d <ejsmajestic@gmail.com>
2025-10-22 08:37:54 -0400

    some NEXT_PUBLIC_BASE_URL is undefined so remove check

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -16,6 +16,6 @@ import { port, str } from "envalid/dist/validators";
 export default cleanEnv(process.env, {
     NODE_ENV: str({ choices: ['development', 'production'] }),
     MONGODB_URI: str(),
-    NEXT_PUBLIC_BASE_URL: str(),
+    // NEXT_PUBLIC_BASE_URL: str(),
     // PORT: port(),
 });
