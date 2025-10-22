7906e3827c968ab72783095d82b41202b08f8762
edman3d <ejsmajestic@gmail.com>
2025-10-22 08:33:33 -0400

    try to set NEXT_PUBLIC env from next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_FRONTEND_URL:
+      process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : 'http://localhost:3000',
+  },
 };
 
 export default nextConfig;
