7655b80f3ed72c295701c2bc7a2869e2c7b2f91e
edman3d <ejsmajestic@gmail.com>
2025-10-22 09:16:48 -0400

    setup env vars to work on localhost, preview and prod

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_BASE_URL:
+    SITE_BASE_URL:
       process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : 'http://localhost:3000',
