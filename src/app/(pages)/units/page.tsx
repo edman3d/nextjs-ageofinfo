6586884296033d8b71862857e304e6af759583fb
edman3d <ejsmajestic@gmail.com>
2025-10-28 07:48:02 -0400

    Display all units on /units pahe. Optional params to avoid fetching units again

--- src/app/(pages)/units/page.tsx
+++ src/app/(pages)/units/page.tsx
@@ -1,12 +1,14 @@
+import UnitsPage from "./UnitsPage";
+
 export const metadata = {
   title: "Units | Age of Info",
 }
 
-export const revalidate = 0; // Must be 0 here because we are fetching from our own internal API
+export const revalidate = 0; // Must be 0 here because we are fetching from our own internal API at build time
 
 export default async function Page() {
 
   return (
-    <div>Units Page</div>
+    <UnitsPage />
   );
 }
